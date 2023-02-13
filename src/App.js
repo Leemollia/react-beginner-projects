ab1334742697763686d07b104cc5b31b4275af09
Mikhail Kozhokar <leemollia@gmail.com>
2023-02-13 19:50:04 +0300

    Users done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import './index.scss';
+import { useEffect, useState } from 'react';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
+import './index.scss';
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [invited, setInvited] = useState(false);
+
+  const result = useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then(res => res.json())
+      .then(result => {
+        setIsLoaded(true);
+        setUsers(result.data);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }, [])
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {invited ?
+        <Success
+        count={selectedUsers.length}
+        setInvited={setInvited}
+        setSelectedUsers={setSelectedUsers} />
+        :
+        <Users
+        key={users.id + users.email}
+        users={users}
+        isLoaded={isLoaded}
+        setInvited={setInvited} 
+        selectedUsers={selectedUsers}
+        setSelectedUsers={setSelectedUsers} />
+        }
     </div>
   );
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,28 +1,61 @@
-import React from 'react';
-import { Skeleton } from './Skeleton';
-import { User } from './User';
+import { useState } from "react";
+import { Skeleton } from "./Skeleton";
+import { User } from "./User";
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({ users = [], isLoaded, setInvited, selectedUsers, setSelectedUsers }) => {
+  const [query, setQuery] = useState("");
+
+  const addToInviteList = (id) => {
+    setSelectedUsers(() => {
+      if (selectedUsers.includes(id)) {
+        return selectedUsers.filter((item) => item !== id);
+      }
+      return [...selectedUsers, id];
+    });
+  };
+
+  const addToInviteUsers = () => {
+    if (!selectedUsers.length) return;
+    setInvited(true);
+  }
+
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input
+          type="text"
+          placeholder="Найти пользователя..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
       </div>
-      {isLoading ? (
+      {isLoaded ? (
+        <ul className="users-list">
+          {users
+            .filter((user) =>
+              user.first_name.concat(user.last_name, user.email).includes(query)
+            )
+            .map((user) => (
+              <User
+                key={user.id}
+                user={user}
+                addToInviteList={addToInviteList}
+                selectedUsers={selectedUsers}
+              />
+            ))}
+        </ul>
+      ) : (
         <div className="skeleton-list">
           <Skeleton />
           <Skeleton />
           <Skeleton />
         </div>
-      ) : (
-        <ul className="users-list">
-          <User />
-        </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      <button onClick={addToInviteUsers} className="send-invite-btn">
+        Отправить приглашение
+      </button>
     </>
   );
 };
